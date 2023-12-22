cf34b98ec800e96d76d6b2c9996a23102d36b1c6
Davide Stenner <davide.stenner@cefriel.com>
2023-12-22 09:28:19 +0100

    added explainer and trainer

---
+++ enefit/model/lgbm/explainer.py
@@ -0,0 +1,96 @@
+import os
+import json
+import pickle
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class LgbmExplainer():       
+    def evaluate_score(self) -> None:        
+        # Find best epoch
+        with open(
+            os.path.join(
+                self.experiment_path,
+                'progress_list_lgb.pkl'
+            ), 'rb'
+        ) as file:
+            progress_list = pickle.load(file)
+
+        progress_dict = {
+            'time': range(self.params_lgb['n_round']),
+        }
+
+        progress_dict.update(
+                {
+                    f"{self.metric_eval}_fold_{i}": progress_list[i]['valid'][self.metric_eval]
+                    for i in range(self.n_fold)
+                }
+            )
+
+        progress_df = pd.DataFrame(progress_dict)
+        progress_df[f"average_{self.metric_eval}"] = progress_df.loc[
+            :, [self.metric_eval in x for x in progress_df.columns]
+        ].mean(axis =1)
+        
+        progress_df[f"std_{self.metric_eval}"] = progress_df.loc[
+            :, [self.metric_eval in x for x in progress_df.columns]
+        ].std(axis =1)
+
+        best_epoch_lgb = int(progress_df[f"average_{self.metric_eval}"].argmin())
+        best_score_lgb = progress_df.loc[
+            best_epoch_lgb,
+            f"average_{self.metric_eval}"
+        ]
+        lgb_std = progress_df.loc[
+            best_epoch_lgb, f"std_{self.metric_eval}"
+        ]
+
+        print(f'Best epoch: {best_epoch_lgb}, CV-Auc: {best_score_lgb:.5f} ± {lgb_std:.5f}')
+
+        self.best_result = {
+            'best_epoch': best_epoch_lgb+1,
+            'best_score': best_score_lgb
+        }
+
+        with open(
+            os.path.join(
+                self.experiment_path,
+                'best_result_lgb.txt'
+            ), 'w'
+        ) as file:
+            json.dump(self.best_result, file)
+        
+    def get_feature_importance(self) -> None:
+   
+        with open(
+            os.path.join(
+                self.experiment_path,
+                'model_list_lgb.pkl'
+            ), 'rb'
+        ) as file:
+            model_list = pickle.load(file)
+
+        feature_importances = pd.DataFrame()
+        feature_importances['feature'] = self.feature_list
+
+        for fold_, model in enumerate(model_list):
+            feature_importances[f'fold_{fold_}'] = model.feature_importance(
+                importance_type='gain', iteration=self.best_result['best_epoch']
+            )
+
+        feature_importances['average'] = feature_importances[
+            [f'fold_{fold_}' for fold_ in range(self.n_fold)]
+        ].mean(axis=1)
+
+        fig = plt.figure(figsize=(12,8))
+        sns.barplot(data=feature_importances.sort_values(by='average', ascending=False).head(50), x='average', y='feature')
+        plt.title(f"50 TOP feature importance over {self.n_fold} average")
+
+        fig.savefig(
+            os.path.join(self.experiment_path, 'importance_plot.png')
+        )
+        plt.close(fig)
+        
+    def explain_model(self) -> None:
+        self.evaluate_score()
+        self.get_feature_importance()
