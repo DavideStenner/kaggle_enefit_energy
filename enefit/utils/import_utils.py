ae575593b2426d62f93e50efb013934943a4d534
Davide Stenner <davide.stenner@cefriel.com>
2023-12-22 09:58:00 +0100

    import params

--- enefit/utils/import_utils.py
+++ enefit/utils/import_utils.py
@@ -3,6 +3,11 @@ import json
 
 from typing import Dict, Any
 
+def import_params(model: str, params_path: str='config/params_{model}.json') -> Dict[str, Any]:
+    with open(params_path.format(model=model)) as params_file:
+        params = json.load(params_file)
+    return params
+
 def import_config(config_path: str='config/config.json') -> Dict[str, Any]:
     with open(config_path) as config_file:
         config = json.load(config_file)
--- train.py
+++ train.py
@@ -1,23 +1,13 @@
 if __name__=='__main__':
-    from enefit.utils.import_utils import import_config
+    from enefit.utils.import_utils import import_config, import_params
     from enefit.model.lgbm import ModelPipeline
 
     config_dict = import_config()
+    params_model = import_params()
+    
     trainer = ModelPipeline(
         experiment_name='test',
-        params_lgb={
-            "boosting_type": "gbdt",
-            "objective": "mae",
-            "n_jobs": -1,
-            "num_leaves": 256,
-            "learning_rate": 0.1,
-            "feature_fraction": 0.75,
-            "bagging_freq": 1,
-            "bagging_fraction": 0.80,
-            "lambda_l2": 1,
-            "verbosity": -1,
-            "n_round": 200
-        },
+        params_lgb=params_model,
         config_dict=config_dict
     )
     trainer.train()
