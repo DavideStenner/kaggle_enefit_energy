70ea5a2d8444cc8c09b0bbe0f5e9f42009369c76
Davide Stenner <davide.stenner@cefriel.com>
2024-01-05 15:41:15 +0100

    fix

--- enefit/preprocess/initialization.py
+++ enefit/preprocess/initialization.py
@@ -1,11 +1,14 @@
 from typing import Any
 
 class EnefitInit():
-    def __init__(self, config_dict: dict[str, Any], target_n_lags: int, agg_target_n_lags: list[int] = [2, 3, 7, 15]):
+    def __init__(self, 
+            config_dict: dict[str, Any], target_n_lags: int, 
+            agg_target_n_lags: list[int] = [2, 3, 7, 15], embarko_skip: int=60
+        ):
         
         self.target_n_lags: int = target_n_lags
         self.agg_target_n_lags: int = agg_target_n_lags
         self.path_original_data: str = config_dict['PATH_ORIGINAL_DATA']
         self.config_dict: dict[str, Any] = config_dict
-        self.embarko_skip: int = 90
+        self.embarko_skip: int = embarko_skip
         self.n_folds: int = config_dict['N_FOLD']
